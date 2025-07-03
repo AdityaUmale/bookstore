e086ce1681cc798e1be3dbafc397b4fbc72a1e41
Aditya prashant umale <aditya.umale17@gmail.com>
2025-07-03 14:06:07 +0530

    book apis

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -40,3 +40,67 @@ export const createBook = async (req: AuthRequest, res: Response) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+
+export const getBooks = async (req: AuthRequest, res: Response) => {
+    try {
+        const books: Book[] = await readJSON<Book>(BOOKS_FILE);
+        const filteredBooks = books.filter((book) => book.userId === req.user!.id);
+        res.status(200).json({ books });
+    } catch (err) {
+        console.error("Error in getBooks:", err);
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+export const getBookById = async (req: AuthRequest, res: Response) => {
+    try {
+        const books: Book[] = await readJSON<Book>(BOOKS_FILE);
+        const book = books.find((book) => book.id === req.params.id);
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+        res.status(200).json({ book });
+    } catch (err) {
+        console.error("Error in getBookById:", err);
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+export const updateBook = async (req: AuthRequest, res: Response) => {
+    try {
+        const books: Book[] = await readJSON<Book>(BOOKS_FILE);
+        const book = books.find((book) => book.id === req.params.id);
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+        const { title, author, genre, publishedYear } = req.body;
+        if (!title || !author || !genre || !publishedYear) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        book.title = title;
+        book.author = author;
+        book.genre = genre;
+        book.publishedYear = publishedYear;
+        await writeJSON<Book>(BOOKS_FILE, books);
+        res.status(200).json({ message: "Book updated", book });
+    } catch (err) {
+        console.error("Error in updateBook:", err);
+    }
+}
+
+export const deleteBook = async (req: AuthRequest, res: Response) => {
+    try {
+        const books: Book[] = await readJSON<Book>(BOOKS_FILE);
+        const book = books.find((book) => book.id === req.params.id);
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+        const filteredBooks = books.filter((book) => book.id !== req.params.id);
+        await writeJSON<Book>(BOOKS_FILE, filteredBooks);
+        res.status(200).json({ message: "Book deleted" });
+    } catch (err) {
+        console.error("Error in deleteBook:", err);
+        res.status(500).json({ message: "Server error" });
+    }
+}
--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createBook } from "../controllers/book.controller";
+import { createBook, deleteBook, getBookById, getBooks, updateBook } from "../controllers/book.controller";
 import { authenticate } from "../middleware/auth.middleware";
 
 const router = Router();
@@ -7,5 +7,21 @@ const router = Router();
 router.post("/create", authenticate, (req, res, next) => {
   createBook(req, res).catch(next);
 }); // ✅ Protect the route
+
+router.get("/books", authenticate, (req, res, next) => {
+  getBooks(req, res).catch(next);
+});
+
+router.get("/books/:id", authenticate, (req, res, next) => {
+  getBookById(req, res).catch(next);
+});
+
+router.put("/books/:id", authenticate, (req, res, next) => {
+  updateBook(req, res).catch(next);
+});
+
+router.delete("/books/:id", authenticate, (req, res, next) => {
+  deleteBook(req, res).catch(next);
+});
 
 export default router;
